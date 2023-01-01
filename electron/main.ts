07037e228d0ee8be70e760251e92293d1fb86d9d
yami <sukoonkumar2@gmail.com>
2023-01-01 21:53:14 +0530

    lil fix in "on second instance"

--- electron/main.ts
+++ electron/main.ts
@@ -267,8 +267,7 @@ app.on("second-instance", (event, commandLine) => {
     if (commandLine.length >= 3) {
         // for file explorer option
         if (fs.existsSync(commandLine[2])) createWindow(commandLine[2]);
-    }
-    if (commandLine.length <= 2 || commandLine.includes("--new-window")) {
+    } else if (commandLine.length <= 2 || commandLine.includes("--new-window")) {
         log.log("second instance detected, opening new window...");
         createWindow();
     }
