7b92af82e05177a147ee2cc9ee935636f39bf451
yami <sukoonkumar2@gmail.com>
2023-01-02 16:08:16 +0530

    better make

--- makeAll.js
+++ makeAll.js
@@ -11,15 +11,22 @@ if (fs.existsSync("./out/full")) {
     })
 }
 fs.mkdirSync("./out/full")
-const printRed = (string) => {
-    console.log(`\x1b[31m${string}\x1b[0m`)
+const printProcessing = (string) => {
+    let times = 0
+    return setInterval(() => {
+        times++
+        process.stdout.clearLine()
+        process.stdout.cursorTo(0)
+        process.stdout.write(string + "".padEnd(times % 4, "."))
+    }, 500);
+    // console.log(`\x1b[31m${string} \x1b[0m`)
 }
 const makeExe = () => {
     fs.writeFileSync("./electron/IS_PORTABLE.ts", `
     const isPortable = false;
     export default isPortable;
     `)
-    printRed('making .exe')
+    const a = printProcessing('making exe ')
     // console.log(`./out/make/squirrel.windows/ia32/Manga Reader-${pkgJSON.version} Setup.exe`)
     const yarnSpawn = exec("yarn makeExe")
     // yarnSpawn.stdout.on('data', (data) => {
@@ -29,18 +36,19 @@ const makeExe = () => {
         process.stdout.write(data)
     });
     yarnSpawn.on('close', (code) => {
+        clearInterval(a)
         console.log(`spawn yarn child process exited with code ${code}.`);
         if (fs.existsSync(`./out/make/squirrel.windows/ia32/Manga Reader-${pkgJSON.version} Setup.exe`)) {
-            printRed(".exe create successfully")
-            printRed("moving .exe ...")
+            console.log(".exe create successfully")
+            console.log("moving .exe ...")
             fs.rename(`./out/make/squirrel.windows/ia32/Manga Reader-${pkgJSON.version} Setup.exe`,
                 `./out/full/Manga.Reader-${pkgJSON.version}-Setup.exe`, (err) => {
                     if (err) return console.error(err)
-                    printRed("moved successfully.")
+                    console.log("moved successfully.")
                     makeZip()
                 })
         } else {
-            printRed(".exe not found")
+            console.log(".exe not found")
         }
     });
 
@@ -50,7 +58,7 @@ const makeZip = () => {
     const isPortable = true;
     export default isPortable;
     `)
-    printRed("making .zip")
+    const a = printProcessing("making zip ")
     // console.log(`./out/make/zip/win32/ia32/Manga.Reader-win32-${pkgJSON.version}-Portable.zip`)
     const yarnSpawn = exec("yarn makeZip")
     // yarnSpawn.stdout.on('data', (data) => {
@@ -60,21 +68,22 @@ const makeZip = () => {
         process.stdout.write(data)
     });
     yarnSpawn.on('close', (code) => {
-        printRed(`spawn yarn child process exited with code ${code}.`);
+        clearInterval(a)
+        console.log(`spawn yarn child process exited with code ${code}.`);
         if (fs.existsSync(`./out/make/zip/win32/ia32/Manga Reader-win32-ia32-${pkgJSON.version}.zip`)) {
-            printRed(".zip create successfully")
-            printRed("moving .zip...")
+            console.log(".zip create successfully")
+            console.log("moving .zip...")
             fs.rename(`./out/make/zip/win32/ia32/Manga Reader-win32-ia32-${pkgJSON.version}.zip`,
                 `./out/full/Manga.Reader-win32-${pkgJSON.version}-Portable.zip`, (err) => {
                     if (err) return console.error(err)
-                    printRed("moved successfully.")
+                    console.log("moved successfully.")
                     fs.writeFileSync("./electron/IS_PORTABLE.ts", `
                     const isPortable = false;
                     export default isPortable;
                     `)
                 })
         } else {
-            printRed(".zip not found")
+            console.log(".zip not found")
         }
     });
 
