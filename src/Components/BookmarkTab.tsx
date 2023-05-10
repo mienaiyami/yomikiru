c4ee8ece36eba5714e177cb4100ed6a730657e77
yami <mienaiyami0@gmail.com>
2023-05-11 03:18:52 +0530

    added: search for bookmark and history

--- src/Components/BookmarkTab.tsx
+++ src/Components/BookmarkTab.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import { useAppSelector } from "../store/hooks";
 import BookmarkHistoryListItem from "./BookmarkHistoryListItem";
 
 const BookmarkTab = () => {
     const bookmarks = useAppSelector((store) => store.bookmarks);
     const appSettings = useAppSelector((store) => store.appSettings);
+
+    const [filter, setFilter] = useState<string>("");
+    const List = (bookmarkData: ChapterItem[], filter: string) => {
+        return bookmarkData.map((e, i) => {
+            if (new RegExp(filter, "ig").test(e.mangaName)) {
+                return (
+                    <BookmarkHistoryListItem isHistory={false} isBookmark={true} index={i} {...e} key={e.link} />
+                );
+            }
+        });
+    };
     return (
         <div
             className="contTab listCont"
@@ -11,22 +23,34 @@ const BookmarkTab = () => {
             id="bookmarksTab"
         >
             <h2>Bookmarks</h2>
+            <div className="tools">
+                <input
+                    type="text"
+                    name=""
+                    spellCheck={false}
+                    placeholder="Type to Search"
+                    // tabIndex={-1}
+                    onChange={(e) => {
+                        const val = e.target.value;
+                        let filter = "";
+                        for (let i = 0; i < val.length; i++) {
+                            filter += val[i] + ".*";
+                        }
+                        setFilter(filter);
+                    }}
+                    onKeyDown={(e) => {
+                        e.stopPropagation();
+                        if (/\[|\]|\(|\)|\*|\+|\?/gi.test(e.key)) {
+                            e.preventDefault();
+                        }
+                        if (e.key === "Escape") {
+                            e.currentTarget.blur();
+                        }
+                    }}
+                />
+            </div>
             <div className="location-cont">
-                {bookmarks.length === 0 ? (
-                    <p>No Bookmarks...</p>
-                ) : (
-                    <ol>
-                        {bookmarks.map((e, i) => (
-                            <BookmarkHistoryListItem
-                                isHistory={false}
-                                isBookmark={true}
-                                index={i}
-                                {...e}
-                                key={e.link}
-                            />
-                        ))}
-                    </ol>
-                )}
+                {bookmarks.length === 0 ? <p>No Bookmarks...</p> : <ol>{List(bookmarks, filter)}</ol>}
             </div>
         </div>
     );
--- src/Components/HistoryTab.tsx
+++ src/Components/HistoryTab.tsx
@@ -3,11 +3,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { deleteAllHistory } from "../store/history";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import BookmarkHistoryListItem from "./BookmarkHistoryListItem";
+import { useState } from "react";
 
 const HistoryTab = () => {
     const history = useAppSelector((store) => store.history);
     const appSettings = useAppSelector((store) => store.appSettings);
     const dispatch = useAppDispatch();
+
+    const [filter, setFilter] = useState<string>("");
+    const List = (historyData: HistoryItem[], filter: string) => {
+        return historyData.map((e, i) => {
+            if (new RegExp(filter, "ig").test(e.mangaName)) {
+                return (
+                    <BookmarkHistoryListItem isBookmark={false} isHistory={true} index={i} {...e} key={e.date} />
+                );
+            }
+        });
+    };
     return (
         <div
             className="contTab listCont"
@@ -35,22 +47,35 @@ const HistoryTab = () => {
                     <FontAwesomeIcon icon={faTrash} />
                 </button>
             </h2>
+
+            <div className="tools">
+                <input
+                    type="text"
+                    name=""
+                    spellCheck={false}
+                    placeholder="Type to Search"
+                    // tabIndex={-1}
+                    onChange={(e) => {
+                        const val = e.target.value;
+                        let filter = "";
+                        for (let i = 0; i < val.length; i++) {
+                            filter += val[i] + ".*";
+                        }
+                        setFilter(filter);
+                    }}
+                    onKeyDown={(e) => {
+                        e.stopPropagation();
+                        if (/\[|\]|\(|\)|\*|\+|\?/gi.test(e.key)) {
+                            e.preventDefault();
+                        }
+                        if (e.key === "Escape") {
+                            e.currentTarget.blur();
+                        }
+                    }}
+                />
+            </div>
             <div className="location-cont">
-                {history.length === 0 ? (
-                    <p>No History...</p>
-                ) : (
-                    <ol>
-                        {history.map((e, i) => (
-                            <BookmarkHistoryListItem
-                                isBookmark={false}
-                                isHistory={true}
-                                index={i}
-                                {...e}
-                                key={e.date}
-                            />
-                        ))}
-                    </ol>
-                )}
+                {history.length === 0 ? <p>No History...</p> : <ol>{List(history, filter)}</ol>}
             </div>
         </div>
     );
